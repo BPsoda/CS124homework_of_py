da5b6c189181b55728407171a8654f9903ca47c6
wo-shi-nibaba <75060591+wo-shi-nibaba@users.noreply.github.com>
2020-12-03 21:13:57 +0800

    Add files via upload

--- 大作业.py
+++ 大作业.py
@@ -11,21 +11,65 @@ class NumList:
     
     def add(self, other):
         if self.sign and other.sign:
-            self.num = addList(self.num, other.num)
-            self.sign=True
+            sum = addList(self.num, other.num)
         elif self.sign == False and other.sign == False:
-            self.num = addList(self.num, other.num)
-            self.sign=False
-        else:
-            pass
-            
-
-        
+            sum = addList(self.num, other.num)
+        elif self.sign == False and other.sign == True:
+            if compare(self.num, other.num):
+                self.sign = False
+                sum = subList(self.num, other.num)
+            else:
+                self.sign = True
+                sum = subList(other.num, self.num)
+        elif self.sign == True and other.sign == False:
+            if compare(self.num, other.num):
+                self.sign = True
+                sum = subList(self.num, other.num)
+            else:
+                self.sign = False
+                sum = subList(other.num ,self.num)
+        if sum==[0]:
+            return [0]
+        sum.append(self.sign)
+        return sum
+                    
     def sub(self, other):
-        pass
+        if self.sign and other.sign:
+            if compare(self.num, other.num):
+                self.sign = True
+                mina = subList(self.num, other.num)
+            else:
+                self.sign = False
+                mina = subList(other.num, self.num)
+        elif self.sign == False and other.sign == False:
+            if compare(self.num, other.num):
+                self.sign = False
+                mina = subList(self.num, other.num)
+            else:
+                self.sign = True
+                mina = subList(other.num, self.num)
+        elif self.sign == False and other.sign == True:
+            mina = addList(self.num, other.num)
+        elif self.sign == True and other.sign == False:
+            mina = addList(self.num, other.num)
+        if mina ==[0]:
+            return [0]
+        mina.append(self.sign)
+        return mina
 
     def mul(self, other):
-        pass
+        if self.sign and other.sign:
+            prod= mulList(self.num, other.num)
+        elif self.sign == False and other.sign == False:
+            prod = mulList(self.num, other.num)
+            self.sign=True
+        else:
+            prod=mulList(self.num,other.num)
+            self.sign=False
+        if prod[-1] ==0:
+            return [0]
+        prod.append(self.sign)
+        return prod
 
     def div(self, other):
         pass
@@ -33,7 +77,7 @@ class NumList:
     def pow(self, power):
         pass
 
-def addList(a, b): # add two positive numlists(without the initial bool)
+def addList(a, b): # adds two positive numlists
     result = []
     carryFlag = False
     for i in range(min(len(a), len(b))):
@@ -66,16 +110,47 @@ def addList(a, b): # add two positive numlists(without the initial bool)
         result.append(1)
     return result
 
-def subList(a, b):
-    pass
+def subList(a, b): # substracts a small positive list number from a big one
+    result=[]
+    for i in range(len(b)):
+        if a[i]<b[i]:
+            a[i]+=10
+            a[i+1]-=1
+        result.append(a[i]-b[i])
+    for i in range(len(b),len(a)-1):
+        if a[i]<0:
+            a[i]+=10
+            a[i+1]-=1
+        result.append(a[i])
+    while len(result)>1:
+        if result[-1]==0:
+            result.pop()
+        else:
+            break
+    return result
 
-def str2list(stri): #turns a string number into a list, adds a bool as the first element to state if it's pos
+def mulList(a,b): #multiplies two numlists
+    result=[]
+    carryFlag=0
+    for i in range(len(b)):
+        temp=[0]*i+[]
+        carryFlag=0
+        for j in range(len(a)):
+            newDigit=a[j]*b[i]+carryFlag
+            carryFlag=newDigit//10
+            newDigit=newDigit%10
+            temp.append(newDigit)
+        if carryFlag>0:
+            temp.append(carryFlag)
+        result=addList(result,temp)
+    return result
+
+def str2list(stri): #turns a string number into a list
+    lst=[]
     if stri[0] == "-":
-        lst=[]
         for i in stri[-1: 0: -1]:
             lst.append(int(i))
     else:
-        lst=[]
         for i in stri[-1:: -1]:
             lst.append(int(i))
     return lst
@@ -85,3 +160,19 @@ def list2str(lst,sign): #turns a list number into a string
         return "-" + "".join(str(i) for i in lst[-1: :-1])
     else:
         return "".join(str(i) for i in lst[-1: :-1])
+
+def compare(a, b): #returns True if a is bigger , False if b is bigger 
+    if len(a) > len(b):
+        return True
+    elif len(a) < len(b):
+        return False
+    else:
+        for i in range(len(a)):
+            if a[-1 * i] < b[-1 * i]:
+                return True
+            elif a[-1 * i] > b[-1 * i]:
+                return False
+        return True 
+n=NumList("999")
+t=NumList("-1000")
+print(n.add(t))
